ebdfd4dc5c54cb817190483b3fd359825ab70d7e
00kl <lucasmoraesplay@gmail.com>
2023-06-21 18:05:29 -0300

    init

---
+++ tests/test-1.spec.ts
@@ -0,0 +1,12 @@
+import { test, expect } from '@playwright/test';
+
+test('test', async ({ page }) => {
+  await page.goto('https://inf15979.disciplinas.rlaiola.inf.ufes.br/boca/');
+  await page.locator('input[name="name"]').click();
+  await page.locator('input[name="name"]').fill('ihc');
+  await page.locator('input[name="password"]').click();
+  await page.locator('input[name="password"]').fill('ihc');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await page.getByRole('cell', { name: 'Username: (site=1)' }).click();
+  await page.getByRole('cell', { name: 'Username: (site=1)' }).click();
+});
